25bfcbdd39c27f7afd955697cb7dea67f162881d
Art <4998038+Alorel@users.noreply.github.com>
2024-07-26 19:55:11 +0100

    chore: rm dead internal `IdbRequestLike` trait

--- src/request/request_like.rs
+++ src/request/request_like.rs
@@ -1,15 +1,6 @@
 use wasm_bindgen::prelude::*;
-use web_sys::{DomException, IdbOpenDbRequest, IdbRequest, IdbRequestReadyState};
 
 use crate::idb_database::IdbVersionChangeEvent;
-
-pub(crate) trait IdbRequestLike {
-    fn get_error(&self) -> Result<Option<DomException>, JsValue>;
-    fn get_result(&self) -> Result<JsValue, JsValue>;
-    fn set_on_error(&self, value: Option<&::js_sys::Function>);
-    fn set_on_success(&self, value: Option<&::js_sys::Function>);
-    fn get_ready_state(&self) -> IdbRequestReadyState;
-}
 
 /// Common trait for `IdbOpenDbRequest`s
 pub trait IdbOpenDbRequestLike {
@@ -23,37 +14,3 @@ pub trait IdbOpenDbRequestLike {
     where
         F: Fn(&IdbVersionChangeEvent) -> Result<(), JsValue> + 'static;
 }
-
-macro_rules! impl_request_like {
-    ($tgt: ident) => {
-        impl IdbRequestLike for $tgt {
-            #[inline]
-            fn set_on_error(&self, value: Option<&::js_sys::Function>) {
-                self.set_onerror(value)
-            }
-
-            #[inline]
-            fn get_ready_state(&self) -> web_sys::IdbRequestReadyState {
-                self.ready_state()
-            }
-
-            #[inline]
-            fn set_on_success(&self, value: Option<&::js_sys::Function>) {
-                self.set_onsuccess(value)
-            }
-
-            #[inline]
-            fn get_error(&self) -> Result<Option<DomException>, JsValue> {
-                self.error()
-            }
-
-            #[inline]
-            fn get_result(&self) -> Result<JsValue, JsValue> {
-                self.result()
-            }
-        }
-    };
-}
-
-impl_request_like!(IdbRequest);
-impl_request_like!(IdbOpenDbRequest);
