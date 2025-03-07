e904a091bf1c3d8d6caaa5f903c0e5d8954bd52e
Sean Aye <sean.aye@agilebits.com>
2025-03-07 12:41:25 -0400

    test: addput complex objects

--- tests/tests/object_store/add_put.rs
+++ tests/tests/object_store/add_put.rs
@@ -59,6 +59,52 @@ macro_rules! common_tests {
             let res = store.$method(1u8).with_key(expect_str.as_str()).await;
             assert_eq!(res, Ok(expect_str));
         }
+
+        /// Verify that we get objects and not maps as output
+        #[cfg(feature = "serde")]
+        #[wasm_bindgen_test]
+        pub async fn serde_object_nesting() {
+            let db = random_db_with_init(move |_, db| {
+                db.create_object_store(&db.name())
+                    .with_key_path("foo".into())
+                    .build()?;
+                Ok(())
+            })
+            .await;
+            open_tx!(db, Readwrite > (tx, store));
+
+            #[derive(serde::Serialize, serde::Deserialize)]
+            struct Foo {
+                foo: String,
+            }
+
+            #[derive(serde::Serialize, serde::Deserialize)]
+            struct Bar {
+                bar: String,
+            }
+
+            #[derive(serde::Serialize, serde::Deserialize)]
+            struct Entry {
+                #[serde(flatten)]
+                key: Foo,
+                value: Bar,
+            }
+
+            let res = store
+                .$method(Entry {
+                    key: Foo {
+                        foo: "foo".to_string(),
+                    },
+                    value: Bar {
+                        bar: "bar".to_string(),
+                    },
+                })
+                .serde()
+                .unwrap()
+                .await;
+
+            assert_eq!(res, Ok(()));
+        }
     };
 }
 
